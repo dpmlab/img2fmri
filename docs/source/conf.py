6823da6024ecf8195a9b820a889fab1f5ac087d6
Max Bennett <maxwellbbennett@gmail.com>
2023-08-01 17:22:29 -0400

    v1.0 config files

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2023, Maxwell Bennett'
 author = 'Maxwell Bennett'
 
 # The full version, including alpha/beta/rc tags
-release = '0.2.dev5'
+release = '1.0'
 
 
 # -- General configuration ---------------------------------------------------
