cbb2c2b1b2ce8ee760c301d466fa35f411d9b733
Halle Dimsdale-Zucker <halledz@iucr.edu>
2025-03-31 14:59:07 -0700

    modify how looping over directories and rois

--- img2fmri_commcon_adapted-from-ipynb-example.py
+++ img2fmri_commcon_adapted-from-ipynb-example.py
@@ -5,10 +5,18 @@ import os, sys, wget, zipfile
 ALL_ROIS = ["EarlyVis", "OPA", "LOC", "RSC", "PPA"]
 roi_list = ALL_ROIS
 
-input_dir = '/data/commcon/stimuli/frames_by_video/adopt-a-pet_resized_1280-720_frames/'
+frames_dir = '/data/commcon/stimuli/frames_by_video/'
+fits_dir = '/data/commcon/stimuli/output_by_video/'
 
-for roi in ALL_ROIS:
-    print(f'Predicting commcon images for {roi}:', file=sys.stderr)
-    roi_output = f'data/commcon_{roi}/'
-    img2fmri.predict(input_data=input_dir, output_dir=roi_output, roi_list=[roi])
-    if roi != ALL_ROIS[-1]: print('\n', file=sys.stderr)
+for dirpath, dirnames, filenames  in os.walk(frames_dir): 
+    for ifolder in dirnames:
+        print("\nworking on {}".format(ifolder))
+        input_path = os.path.join(frames_dir, ifolder)
+        output_path= os.path.join(fits_dir, ifolder)
+        for roi in ALL_ROIS:
+            print(f'\tPredicting commcon images for {roi}:', file=sys.stderr)
+            roi_output = os.path.join(output_path, '{}'.format(roi))
+            #print('\tinput_path is {}'.format(input_path))
+            #print('\troi_output is {}'.format(roi_output))
+            img2fmri.predict(input_data=input_path, output_dir=roi_output, roi_list=[roi])
+            if roi != ALL_ROIS[-1]: print('\n', file=sys.stderr)
